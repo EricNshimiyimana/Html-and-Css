afc74d5f77041d372711c5ce0ef7ed521ce1fe8a
Esther Wachuka <estherw8525@gmail.com>
2018-03-20 13:25:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("myCarousel");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 1000); 
+}
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
